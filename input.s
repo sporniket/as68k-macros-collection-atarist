9b83b74ca31e3a3cfc5bc280f7f09ddd4c368592
David SPORN <sporniket.studio@gmail.com>
2023-03-02 07:16:22 +0100

    macros using KBDVBASE

--- input.s
+++ input.s
@@ -52,20 +52,61 @@ FlushInp                macro
 .thatsAll\@
                         endm
 
+; ================================================================================================================
+; ================================================================================================================
+; IKBD handlers management
+; ================================================================================================================
+; KBDVBASE -- see https://freemint.github.io/tos.hyp/en/xbios_structures.html#KBDVBASE
+;
+                        rsreset
+
+KBDVBASE_midivec        rs.l 1 ; MIDI interrupt vector
+KBDVBASE_vkbderr        rs.l 1 ; Keyboard error vector
+KBDVBASE_vmiderr        rs.l 1 ; MIDI error vector
+KBDVBASE_statvec        rs.l 1 ; Keyboard status
+KBDVBASE_mousevec       rs.l 1 ; Keyboard mouse status
+KBDVBASE_clockvec       rs.l 1 ; Keyboard clock
+KBDVBASE_joyvec         rs.l 1 ; Keyboard joystick status
+KBDVBASE_midisys        rs.l 1 ; System Midi vector
+KBDVBASE_kbdsys         rs.l 1 ; Keyboard vector
+KBDVBASE_drvstat        rs.b 1 ; Keyboard driver status
+
+SIZEOF_KBDVBASE         rs.b 1 ; WITH EVEN FIX PADDING
+EVENSIZEOF_KBDVBASE     rs.b 0 ; 
+
+; ================================================================================================================
+; Wait for idle state
+;
+WaitForIdleIkbdHandlers macro
+                        ;1 - address registry having KBDVBASE
+.waitIkbd\@             tst.b                   KBDVBASE_drvstat(\1)
+                        bne.s                   .waitIkbd\@
+                        endm
+
+; ================================================================================================================
+; Save individual handler
+;
 SaveSysIkbdHandler      macro
                         ;1 - storage address of the structure
                         ;2 - storage address for the default handler for joystick
                         ;3 - adresse registry to use (side effect)
+                        ;4 - offset of handler to save in KBDVBASE
                         _xos_Kbdvbase
                         move.l                  d0,\1
                         move.l                  d0,\3
-                        move.l                  24(\3),\2
+                        move.l                  \4(\3),\2
                         endm
 
+; ================================================================================================================
+; Restore individual handler
+;
 RestoreSysIkbdHandler   macro
                         ;1 - storage address of the structure
                         ;2 - storage address for the default handler for joystick
                         ;3 - adresse registry to use (side effect)
+                        ;4 - offset of handler to save in KBDVBASE
                         move.l                  \1,\3
-                        move.l                  \2,24(\3)
+                        move.l                  \2,\4(\3)
                         endm
+
+; ================================================================================================================
